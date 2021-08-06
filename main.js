d99e7eb24fad874f6a68095368c33464f8731313
wkbys249 <wkbys.yuto@gmail.com>
2021-08-06 17:35:55 +0900

    見た目変更

--- main.js
+++ main.js
@@ -1,71 +1,61 @@
 "use strict";
-/**
- * 見た目とか機能に一切納得がいっていない
- * 入力欄に文字を入力 Enterで追加 削除ボタンで削除
- */
 
 let count = 0;
 const todolist = document.getElementById("todolist");
 const textarea = document.getElementById("textarea");
+const addButton = document.getElementById("addButton");
 
 window.onload = () => {
     textarea.addEventListener("keypress", textareaEvent);
+    addButton.addEventListener("click", createTask);
 }
 
 
 const textareaEvent =(e)=>{
     if(e.keyCode === 13){
-        const text = textarea.value;
-        const id = "task" + count;
-        addTask(id, text);
-        count++;
+        createTask();
     }
 }
 
+const createTask =()=>{
+    const text = textarea.value;
+    const id = "task" + count;
+    addTask(id, text);
+    textarea.value = "";
+    textarea.blur();
+    count++;
+}
+
 const addTask =(id, text)=>{
-    const tr = document.createElement("tr");
-    const textElemenent = document.createElement("p");
-    const timeElemenent = document.createElement("p");
-    const div=document.createElement("div");
-    const deleteBtn = document.createElement("input");
-    const time = getNowTime();
+    const task = document.createElement("li");
+    const textBox = document.createElement("div");
+    const textLabel = document.createElement("input");
+    const deleteButton = document.createElement("input");
 
-    tr.className = "box";
-    textElemenent.className = "el text";
-    timeElemenent.className = "el time";
-    deleteBtn.className = "el deleteBtn";
-    div.className = "el ctrl"
+    task.className = "task top";
+    textBox.className = "text-box";
+    textLabel.className = "text";
+    deleteButton.className = "button delete-button";
 
-    tr.id = id;
-    textElemenent.textContent = text;
-    timeElemenent.textContent = time;
-    deleteBtn.type="button";
-    deleteBtn.value="Delete";
-    deleteBtn.addEventListener("click", ()=>{
+    task.id = id;
+
+    textLabel.type = "text";
+    textLabel.value = text;
+    textLabel.readOnly = true;
+
+    deleteButton.type = "button";
+    deleteButton.value = "×";
+    deleteButton.addEventListener("click", ()=>{
         deleteTask(id);
-    });
+    })
 
-    tr.appendChild(textElemenent);
-    //tr.appendChild(timeElemenent);
-    div.appendChild(deleteBtn);
-    tr.appendChild(div);
-    todolist.insertBefore(tr, todolist.firstChild);
+    textBox.appendChild(textLabel);
+    task.appendChild(textBox);
+    task.appendChild(deleteButton);
+    todolist.insertBefore(task, todolist.children[2]);
 }
 
 const deleteTask =(id)=>{
     const task = document.getElementById(id);
     task.parentNode.removeChild(task);
-}
-
-const getNowTime=()=>{
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth() + 1;
-    const date = now.getDate();
-    const hour = now.getHours();
-    const min = now.getMinutes();
-    const sec = now.getSeconds();
-
-    const text = month + "/" + date + "," + year + "   " + hour + ":" + min + ":" + sec;
-    return text;
 }
